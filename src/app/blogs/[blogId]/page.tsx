39779d51d7cb42465523848768bebda3e3da7a64
ryota-09 <n.ryo950911@gmail.com>
2024-05-03 18:48:55 +0900

    fix: 変更テスト

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -2,8 +2,8 @@ import ArticleBody from "@/components/ArticleBody";
 import { getBlogById, getBlogList } from "@/lib/microcms";
 
 export async function generateStaticParams() {
-  const blogList = await getBlogList({ fields: "id"});
-  
+  const blogList = await getBlogList({ fields: "id" });
+
   return blogList.contents.map((content) => ({
     blogId: content.id
   }))
@@ -18,11 +18,9 @@ type PageProps = {
 const Page = async ({ params }: PageProps) => {
   const data = await getBlogById(params.blogId);
   return (
-    <>
-      <div className="max-w-[1028px] mx-auto bg-white border-2 px-4 py-2">
-        <ArticleBody data={data} />
-      </div>
-    </>
+    <div className="max-w-[1028px] mx-auto bg-white border-2 px-4 py-2">
+      <ArticleBody data={data} />
+    </div>
   );
 }
 export default Page;
