3959e9825cb484f5119dbdf47c009da87acc94e5
ryota-09 <n.ryo950911@gmail.com>
2024-05-17 19:07:38 +0900

    feat; 404ページの作成

---
+++ src/app/not-found.tsx
@@ -0,0 +1,34 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ImageWithLoader from "@/components/UiParts/ImageWithLoader"
+import { Metadata } from "next";
+import Image from "next/image"
+import Link from "next/link"
+
+export const metadata: Metadata = {
+  title: "Not Found"
+};
+
+const Page = () => {
+  return (
+    <>
+      <Header />
+      <main className="flex-grow flex flex-col md:flex-row container mx-auto gap-4 my-4 h-full px-2">
+        <div className="flex w-full flex-col items-center justify-center bg-white px-4 py-12 dark:bg-gray-900 border-2 border-gray-200">
+          <div className="flex flex-col items-center justify-center space-y-4">
+            <ImageWithLoader src="/404.png" alt="No contents" width={300} height={300} sizes="100vw" style={{ width: '50%', height: 'auto' }} />
+            <h1 className="text-8xl font-bold text-gray-800 dark:text-gray-200">404</h1>
+            <p className="text-lg font-medium text-gray-600 dark:text-gray-400">
+              お探しのページが見つかりませんでした。
+            </p>
+            <Link href="/blogs" className="text-base-color border-2 border-base-color px-4 py-2 rounded-md hover:bg-base-color hover:text-white hover:border-base-color transition duration-200">
+              トップページに戻る
+            </Link>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  )
+}
+export default Page
--- src/components/UiParts/NoContentsPage/index.tsx
+++ src/components/UiParts/NoContentsPage/index.tsx
@@ -5,7 +5,7 @@ const NoContents = () => {
     <div className="h-full bg-white border-2 border-gray-200 py-16 px-4 flex justify-center items-center">
       <ImageWithLoader src="/no_contents.png" alt="No contents" width={300} height={300} sizes="100vw" style={{ width: '50%', height: 'auto' }} />
       <div>
-        <p>表示するコンテンツがありません。</p>
+        <p>表示できるコンテンツがありません。</p>
       </div>
     </div>
   );
