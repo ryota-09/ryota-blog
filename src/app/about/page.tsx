2a8c758e4fc393061fb5b5c7fa12427bde98e6cf
ryota-09 <n.ryo950911@gmail.com>
2024-05-18 09:04:50 +0900

    feat: loadingページの雛形作成

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import { AUTHOR_DESCRIPTION, AUTHOR_NAME } from "@/static/blogs"
 
 const Page = () => {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 bg-white border-2 border-gray-200 h-full">
+    <article className="w-full py-12 md:py-24 lg:py-32 bg-white border-2 border-gray-200 h-full">
       <div className="container grid items-center gap-8 px-4 md:px-6 lg:grid-cols-2 lg:gap-12">
         <div className="flex flex-col items-start space-y-8">
           <Chip label={"Software Engineer"} classes="bg-light py-1 px-3 text-txt-base" />
@@ -15,7 +15,7 @@ const Page = () => {
         </div>
         <ImageWithLoader src="/icon.jpg" alt={AUTHOR_NAME} className="block shadow-2xl mx-auto aspect-square overflow-hidden rounded-full object-cover w-[200px] md:w-[400px]" width={400} height={400} />
       </div>
-    </section>
+    </article>
   )
 }
 export default Page
---
+++ src/app/blogs/loading.tsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+  return (
+    <div>
+      <p className="text-3xl text-center font-extrabold">Loading...</p>
+    </div>
+  )
+}
+export default Loading;
