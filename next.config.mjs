8aaebff76e44c0ae015a570a7913f78be014f328
ryota-09 <n.ryo950911@gmail.com>
2024-06-03 10:49:15 +0900

    style: スクロール可能であることを伝えるためにグラデーションを追加

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
   output: "standalone",
   images: {
     "remotePatterns": [{ protocol: "https", hostname: "images.microcms-assets.io" }]
+  },
+  experimental: {
+    optimizeCss: true,
   }
 };
 export default nextConfig
