2ccc53ef248a5fc7dca77d29f0986c429fa77b02
ryota-09 <n.ryo950911@gmail.com>
2024-06-04 13:33:32 +0900

    style: モバイルサイズのcopiesdラベルの位置を修正

--- src/components/UiParts/PopupModal/index.tsx
+++ src/components/UiParts/PopupModal/index.tsx
@@ -1,7 +1,7 @@
 "use client"
+import { useState, type ReactNode } from "react"
 import { useKey } from "@/hooks/keybord"
 import { cltw } from "@/util"
-import { useState, type ReactNode } from "react"
 
 type PopupModalProps = {
   children: ReactNode
