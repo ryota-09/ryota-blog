0755f388ee523789471a64cc51ab728f49d62b5b
ryota-09 <n.ryo950911@gmail.com>
2024-06-07 20:19:02 +0900

    fix: 一時的にパスを修正

--- src/components/UiParts/IssueButton/index.tsx
+++ src/components/UiParts/IssueButton/index.tsx
@@ -6,7 +6,8 @@ type IssueButtonProps = {
 }
 
 const IssueButton = ({ currentPath }: IssueButtonProps) => {
-  const href = `https://github.com/ryota-09/ryota-blog/issues/new?template=issue_template.md&body=対象ページ:+${currentPath}`
+  // const href = `https://github.com/ryota-09/ryota-blog/issues/new?template=issue_template.md&body=対象ページ:+${currentPath}`
+  const href = `https://github.com/ryota-09/ryota-blog/issues/new?template=issue_template.md`
   return (
     <ExternalLink href={href} className="flex gap-4 items-center hover:opacity-70 dark:bg-gray-500 w-[70%] md:w-1/3  lg:w-1/4 border shadow-md p-4">
       <figure>
