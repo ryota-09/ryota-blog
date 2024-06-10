7fa13b543e7f6e3632226e81b792ce480507d7c5
ryota-09 <n.ryo950911@gmail.com>
2024-06-10 11:30:13 +0900

    style: infoラベルの文字の大きさを調整

--- src/components/ArticleBody/RichEditor/CustomUI/CustomLink.tsx
+++ src/components/ArticleBody/RichEditor/CustomUI/CustomLink.tsx
@@ -5,7 +5,7 @@ type CustomLinkProps = ComponentProps<"a">
 
 const CustomLink = ({ href, children, ...restProps }: CustomLinkProps) => {
   return (
-    <Link {...restProps} href={href ?? ""} className=" underline underline-offset-4 transition hover:text-base-color dark:hover:text-primary hover:no-underline">{children}</Link>
+    <Link {...restProps} href={href ?? ""} className=" underline underline-offset-4 transition hover:text-base-color dark:hover:text-primary hover:no-underline break-all">{children}</Link>
   )
 }
 export default CustomLink;
--- src/components/UiParts/InfoYearsCard/index.tsx
+++ src/components/UiParts/InfoYearsCard/index.tsx
@@ -5,8 +5,8 @@ type InfoYearsCardProps = {
 const InfoYearsCard = ({ diffYear }: InfoYearsCardProps) => {
   return (
     <div className="bg-yellow-200 p-2 md:p-4 inline-block">
-      <p className="text-orange-400 font-bold text-md">
-        こちらは <span className="text-2xl underline underline-offset-4">{diffYear}</span> 年前に公開された記事です
+      <p className="text-orange-400 font-bold text-sm md:text-md">
+        こちらは <span className="text-xl md:text-2xl underline underline-offset-4">{diffYear}</span> 年前に公開された記事です
       </p>
     </div>
   )
