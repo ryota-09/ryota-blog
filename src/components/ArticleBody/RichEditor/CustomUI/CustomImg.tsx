7baf2471490eb5fde3b257e6cf66b5be45c63742
ryota-09 <n.ryo950911@gmail.com>
2024-06-10 15:31:52 +0900

    feat: next/imageをGif画像に対応

--- src/components/ArticleBody/RichEditor/CustomUI/CustomImg.tsx
+++ src/components/ArticleBody/RichEditor/CustomUI/CustomImg.tsx
@@ -4,9 +4,11 @@ import { type ComponentProps } from "react";
 type CustomImgProps = { src: string, alt: string, width: string, height: string } & Omit<ComponentProps<"img">, "src" | "alt" | "width" | "height">
 
 const CustomImg = ({ src, alt, width, height, ...restProps }: CustomImgProps) => {
+  // NOTE: next/image はGif画像に対して、最適化が効かない
+  const isGif = src.endsWith(".gif");
   return (
     <p className="flex justify-center my-8">
-      <Image src={src} alt={alt} width={+width} height={+height} className="w-[70%]" {...restProps} />
+      <Image src={src} alt={alt} width={+width} height={+height} unoptimized={isGif} className="w-[70%]"  {...restProps} />
     </p>
   )
 }
