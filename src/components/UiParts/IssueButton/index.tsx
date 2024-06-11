55f6afc1bbd558bda7252606e08d3df209db689f
ryota-09 <n.ryo950911@gmail.com>
2024-06-11 10:54:43 +0900

    style: 修正リクエストボタンの幅を修正

--- src/components/UiParts/IssueButton/index.tsx
+++ src/components/UiParts/IssueButton/index.tsx
@@ -10,7 +10,7 @@ const IssueButton = ({ currentPath }: IssueButtonProps) => {
   const encodedBodyText = encodeURIComponent(bodyText);
   const href = `https://github.com/ryota-09/ryota-blog/issues/new?body=${encodedBodyText}`;
   return (
-    <ExternalLink href={href} className="flex gap-4 items-center hover:opacity-70 dark:bg-gray-500 w-[70%] md:w-1/3  lg:w-1/4 border shadow-md p-4">
+    <ExternalLink href={href} className="flex gap-4 items-center hover:opacity-70 dark:bg-gray-500 w-full sm:w-1/2  lg:w-1/4 border shadow-md p-4">
       <figure>
         <Image src="/icons/github.svg" alt="GitHub" width={24} height={24} />
       </figure>
