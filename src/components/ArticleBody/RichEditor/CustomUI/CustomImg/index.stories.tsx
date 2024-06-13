ce95e8d8dee90af01b3aa0f4bef87e7e5d584dc4
ryota-09 <n.ryo950911@gmail.com>
2024-06-13 12:55:23 +0900

    test: CustomImgのstoryファイルの作成

---
+++ src/components/ArticleBody/RichEditor/CustomUI/CustomImg/index.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import parser from "html-react-parser";
+
+import CustomImg from ".";
+import { customReplaceOptions } from "@/components/ArticleBody/RichEditor/ReplaceUiParts.lib";
+
+const meta = {
+  title: 'RichEditor/Img',
+  component: CustomImg,
+  tags: ['autodocs'],
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  args: {
+    src: "",
+    alt: "",
+    height: "",
+    width: "",
+  },
+} satisfies Meta<typeof CustomImg>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const Template = (args: { html: string }) => <div className="m-8 w-[365px]" >{parser(args.html, customReplaceOptions)}</div>
+
+export const Default: Story = {
+  render: () => {
+    const testHTML = "<img src='/author.jpg' alt='ダミー画像' />"
+    return <Template html={testHTML} />
+  }
+}
