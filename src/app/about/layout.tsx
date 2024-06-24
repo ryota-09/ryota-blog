1626c5a97350b869c6bd9af6e596b075a96a3079
ryota-09 <n.ryo950911@gmail.com>
2024-06-24 14:22:29 +0900

    fix: /aboutページのtitleが重複しないように修正

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { SITE_DESCRIPTION, SITE_TITLE } from "@/static/blogs";
+import { SITE_DESCRIPTION } from "@/static/blogs";
 import { baseURL } from "@/config";
 
 export const metadata: Metadata = {
-  title: `About | ${SITE_TITLE}`,
+  title: 'About',
   description: SITE_DESCRIPTION,
   metadataBase: new URL(baseURL)
 };
