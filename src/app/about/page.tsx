a5daff1768e6d1f9acbcbe53c7fc75defd76f0c8
ryota-09 <n.ryo950911@gmail.com>
2025-05-31 20:16:15 +0900

    fix:作者アイコンの差し替え

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,23 +1,34 @@
-import SocialMediaNav from "@/components/Header/SocialMediaNav"
-import Chip from "@/components/UiParts/Chip"
-import ImageWithBlur from "@/components/UiParts/ImageWithBlur"
-import { AUTHOR_DESCRIPTION, AUTHOR_NAME } from "@/static/blogs"
+import SocialMediaNav from "@/components/Header/SocialMediaNav";
+import Chip from "@/components/UiParts/Chip";
+import ImageWithBlur from "@/components/UiParts/ImageWithBlur";
+import { AUTHOR_DESCRIPTION, AUTHOR_NAME } from "@/static/blogs";
 
 const Page = () => {
   return (
-    <article className="flex-grow flex flex-col md:flex-row items-center justify-center md:justify-between gap-8 w-full min-h-[600px] bg-white dark:bg-black border-2 border-gray-200 dark:border-gray-600 h-full px-8">
+    <article className="flex h-full min-h-[600px] w-full flex-grow flex-col items-center justify-center gap-8 border-2 border-gray-200 bg-white px-8 md:flex-row md:justify-between dark:border-gray-600 dark:bg-black">
       <div className="flex flex-col items-start space-y-8">
-        <Chip label={"Software Engineer"} classes="bg-light dark:bg-primary py-1 px-3 text-txt-base" />
-        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl dark:text-gray-300">{AUTHOR_NAME}</h1>
+        <Chip
+          label={"Software Engineer"}
+          classes="bg-light dark:bg-primary py-1 px-3 text-txt-base"
+        />
+        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl dark:text-gray-300">
+          {AUTHOR_NAME}
+        </h1>
         <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
           {AUTHOR_DESCRIPTION}
         </p>
         <SocialMediaNav />
       </div>
       <figure>
-        <ImageWithBlur src="/author.jpg" alt={AUTHOR_NAME} className="block shadow-2xl mx-auto aspect-square overflow-hidden rounded-full object-cover w-[200px] md:w-[400px]" width={400} height={400} />
+        <ImageWithBlur
+          src="/author.png"
+          alt={AUTHOR_NAME}
+          className="mx-auto block aspect-square w-[200px] overflow-hidden rounded-full object-cover shadow-2xl md:w-[400px]"
+          width={400}
+          height={400}
+        />
       </figure>
     </article>
-  )
-}
-export default Page
+  );
+};
+export default Page;
--- src/components/ArticleBody/BottomCard/index.tsx
+++ src/components/ArticleBody/BottomCard/index.tsx
@@ -4,14 +4,23 @@ import Image from "next/image";
 const BottomCard = () => {
   return (
     <>
-      <div className='flex items-center justify-center shrink-0 mx-6'>
-      <Image src="/author.jpg" alt="author" width={80} height={80} sizes="100vw" className="rounded-full" />
+      <div className="mx-6 flex shrink-0 items-center justify-center">
+        <Image
+          src="/author.png"
+          alt="author"
+          width={80}
+          height={80}
+          sizes="100vw"
+          className="rounded-full"
+        />
       </div>
       <div>
-        <p className='font-bold text-3xl dark:text-gray-300'>{AUTHOR_NAME}</p>
-        <p className="mx-1 mt-4 text-gray-500 dark:text-gray-400">{AUTHOR_DESCRIPTION}</p>
+        <p className="text-3xl font-bold dark:text-gray-300">{AUTHOR_NAME}</p>
+        <p className="mx-1 mt-4 text-gray-500 dark:text-gray-400">
+          {AUTHOR_DESCRIPTION}
+        </p>
       </div>
     </>
-  )
-}
+  );
+};
 export default BottomCard;
