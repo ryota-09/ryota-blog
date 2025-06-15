75abf390a3963fc68f6d3a204d2f906fbd2372f0
ryota-09 <n.ryo950911@gmail.com>
2025-06-15 19:15:39 +0900

    fix:titleを修正

--- src/components/ErrorPageFooter/index.tsx
+++ src/components/ErrorPageFooter/index.tsx
@@ -9,7 +9,7 @@ const ErrorPageFooter = ({ locale }: ErrorPageFooterProps) => {
     <footer className="bg-white dark:bg-black border-t border-gray-200 dark:border-gray-600 py-8">
       <div className="container mx-auto px-2 md:px-0">
         <div className="text-center text-gray-600 dark:text-gray-400">
-          <p>&copy; {currentYear} {locale === 'en' ? "Ryota's Web Engineer Blog" : 'りょうたのWebエンジニアブログ'}</p>
+          <p>&copy; {currentYear} {locale === 'en' ? "Ryota's Web Engineer Blog" : 'Ryota-Blog'}</p>
           <p className="mt-2">All rights reserved.</p>
         </div>
       </div>
