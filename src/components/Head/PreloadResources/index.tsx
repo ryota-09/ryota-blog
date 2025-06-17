5fa3a00dfa7009dda423a39c9b9063472aa27926
ryota-09 <69068250+ryota-09@users.noreply.github.com>
2025-06-17 20:59:26 +0900

    feat: add resource hints

--- src/components/Head/PreloadResources/index.tsx
+++ src/components/Head/PreloadResources/index.tsx
@@ -1,11 +1,23 @@
 'use client'
 import ReactDOM from 'react-dom'
+import { microCMSServiceDomain } from '@/config'
 
 const IMAGE_DOMAIN = 'https://images.microcms-assets.io'
 const PROJECT_ID = '4626924a681346e9a0fcabe5478eb9fa'
+const GTM_DOMAIN = 'https://www.googletagmanager.com'
+const GA_DOMAIN = 'https://www.google-analytics.com'
+const FONTS_API = 'https://fonts.googleapis.com'
+const FONTS_GSTATIC = 'https://fonts.gstatic.com'
 
 const PreloadResources = () => {
+  ReactDOM.preconnect(GTM_DOMAIN)
+  ReactDOM.preconnect(GA_DOMAIN)
+  ReactDOM.preconnect(FONTS_API)
+  ReactDOM.preconnect(FONTS_GSTATIC, { crossOrigin: "anonymous" })
+  ReactDOM.preconnect(`https://${microCMSServiceDomain}.microcms.io`)
+  ReactDOM.preconnect(IMAGE_DOMAIN, { crossOrigin: "anonymous" })
   ReactDOM.preconnect(`${IMAGE_DOMAIN}/assets/${PROJECT_ID}`, { crossOrigin: "anonymous" })
+  ReactDOM.prefetchDNS(GTM_DOMAIN)
   ReactDOM.prefetchDNS(IMAGE_DOMAIN)
   return null
 }
